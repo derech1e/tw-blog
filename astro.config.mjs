eda216f9c54e9790c444bcf876b9e04cedf9b8c5
derech1e <nurkthomas@gmail.com>
2025-08-06 08:37:43 +0200

    remove deprecated stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
