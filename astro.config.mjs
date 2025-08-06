1f6f5c787490e26bf02dc7d34c147f71a0a0a0e4
derech1e <nurkthomas@gmail.com>
2025-08-06 08:25:13 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel({
+    edgeMiddleware: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8
+  }),
+  output: 'static'
 });
