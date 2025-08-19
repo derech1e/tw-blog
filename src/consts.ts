29599658c8292a52da5ff639beb0cb908f8b879a
derech1e <nurkthomas@gmail.com>
2025-08-19 23:13:13 +0200

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Taiwan Blog';
-export const SITE_DESCRIPTION = 'Willkommen auf meiner Webseite!';
+export const SITE_DESCRIPTION = 'Blog über das Auslandssemester in Taiwan an der NTHU in Hsinchu 2025/26';
